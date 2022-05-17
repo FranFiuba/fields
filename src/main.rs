b330698b39ebe5b9060e40dee8f9dbb97faf2c7b
santiago-lambda <santiago.pittella@lambdaclass.com>
2022-05-17 18:12:37 -0300

    add 'add' function

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
+use std::ops::Add;
+
 #[derive(Debug)]
-pub struct FieldElemnt {
+pub struct FieldElement {
     num: u32,
     prime: u32
 }
 
 
-impl FieldElemnt {
+impl FieldElement {
     pub fn new(num: u32, prime: u32) -> Self{
         if num >= prime {
          panic!("Num {} not in field range 0 to {}", num, prime);
@@ -15,21 +17,39 @@ impl FieldElemnt {
     }
 }
 
+impl Add for FieldElement {
+  type Output = Self;
 
-impl PartialEq for FieldElemnt {
+  fn add(self, other: Self) -> Self{
+    if self.prime != other.prime {
+      panic!("Cannot add two numbers in different field");
+    }
+    let num: u32 = (self.num + other.num) % self.prime;
+    let prime: u32 = self.prime;
+    Self {num, prime}
+  }
+}
+
+
+impl PartialEq for FieldElement {
     fn eq(&self, other: &Self) -> bool {
         self.num == other.num && self.prime == other.prime
     }
 }
 
 fn main() {
-   let field : FieldElemnt = FieldElemnt::new(10, 11);
-   let other_field : FieldElemnt = FieldElemnt::new(10, 11);
-   let an_other_field: FieldElemnt = FieldElemnt::new(20, 21);
+   let field : FieldElement = FieldElement::new(10, 11);
+   let other_field : FieldElement = FieldElement::new(10, 11);
+   let an_other_field: FieldElement = FieldElement::new(20, 21);
 
    assert_eq!(field, other_field);
    assert_ne!(field, an_other_field);
+
+   let sum_elem1 : FieldElement = FieldElement::new(1, 10);
+   let sum_elem2 : FieldElement = FieldElement::new(1, 10);
+   let sum_total_hardcoded : FieldElement = FieldElement::new(2, 10);
+   let sum_total : FieldElement = sum_elem1.add(sum_elem2);
+
+   assert_eq!(sum_total, sum_total_hardcoded);
 }
-
-
 
